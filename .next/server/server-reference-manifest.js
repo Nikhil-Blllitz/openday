7fb6e6a1fea77cdeba8f4e2ad197faa493ae05ef
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2025-03-19 20:29:32 +0530

    folder structure

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"apHiGbzYGZL7H14Pgga8Rww7AnZueEHVRoLND2gsuRQ=\"\n}"
---
+++ src/app/RSVP/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>RSVP</h1>
+        <p>Integrate with supabase for getting user info!</p>
+    </div>
+  )
+}
+
+export default page
